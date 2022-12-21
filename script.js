382c94bcc7da0e44c5269767ad23b7c5f5f69389
navintar <106975042+navintarBT@users.noreply.github.com>
2022-12-21 16:43:02 +0700

    tar

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
 const product = [
     {
         id: 0,
-        image: 'image/gg-1.jpg',
-        title: 'Z Flip Foldable Mobile',
-        price: 120,
+        image: 'image/ຂະໜົມປັງທູນ້າ.png',
+        title: 'ແຊນວີດທູນ້າ',
+        price: 30000,
     },
     {
         id: 1,
-        image: 'image/hh-2.jpg',
-        title: 'Air Pods Pro',
-        price: 60,
+        image: 'image/ອາຫານຄີນ.png',
+        title: 'ອາຫານຄີນ',
+        price: 60000,
     },
     {
         id: 2,
-        image: 'image/ee-3.jpg',
-        title: '250D DSLR Camera',
-        price: 230,
+        image: 'image/ສະເຕັກເນື້ອ.png',
+        title: 'ສະເຕັກເນື້ອ',
+        price: 75000,
     },
     {
         id: 3,
-        image: 'image/aa-1.jpg',
-        title: 'Head Phones',
-        price: 100,
+        image: 'image/ເຂົ້າໜຽວໝາກມ່ວງ.png',
+        title: 'ເຂົ້າໜຽວໝາກມ່ວງ',
+        price: 25000,
+    },
+    {
+        id: 4,
+        image: 'image/ລາຊັນຍ້າ2.png',
+        title: 'ຊາລັນຍ້າ',
+        price: 55000,
+    },
+    {
+        id: 5,
+        image: 'image/ເບີເກີ້.png',
+        title: 'ເບີເກີ້',
+        price: 35000,
+    },
+    {
+        id: 3,
+        image: 'image/ມິກເບີລີ້ເຟນໂທສ.png',
+        title: 'ມິກເບີລີ້ເຟນໂທສ',
+        price: 65000,
+    },
+    {
+        id: 6,
+        image: 'image/ສະປາເກັດຕີ້.png',
+        title: 'ສະປາເກັດຕີ້',
+        price: 35000,
     }
 ];
 const categories = [...new Set(product.map((item)=>
@@ -37,8 +61,8 @@ document.getElementById('root').innerHTML = categories.map((item)=>
             </div>
         <div class='bottom'>
         <p>${title}</p>
-        <h2>$ ${price}.00</h2>`+
-        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+        <h2> ${price}ກີບ</h2>`+
+        "<button onclick='addtocart("+(i++)+")'>ກົດສັ່ງຊື້</button>"+
         `</div>
         </div>`
     )
@@ -83,21 +107,21 @@ function displaycart(){
     document.getElementById("count").innerHTML=cart.length;
     if(cart.length==0){
         document.getElementById('cartItem').innerHTML = "Your cart is empty";
-        document.getElementById("total").innerHTML = "$ "+0+".000";
+        document.getElementById("total").innerHTML = "ກີບ "+0+".000";
     }
     else{
         document.getElementById("cartItem").innerHTML = cart.map((items)=>
         {
             var {image, title, price} = items;
             total=total+price;
-            document.getElementById("total").innerHTML = "$ "+total+".000";
+            document.getElementById("total").innerHTML = ""+total+"ກີບ";
             return(
                 `<div class='cart-item'>
                 <div class='row-img'>
                     <img class='rowimg' src=${image}>
                 </div>
                 <p style='font-size:12px;'>${title}</p>
-                <h2 style='font-size: 15px;'>$ ${price}.00</h2>`+
+                <h2 style='font-size: 15px;'> ${price}ກີບ</h2>`+
                 "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
             );
         }).join('');
